f2c55f94b88b9685e9b487ff29865d5b6f795ad6
Alejandro Toledo <atoledofr@gmail.com>
2022-02-09 16:04:12 -0500

    modify firebase config and delete useless files for the project

---
+++ src/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC5Iuu7xpD6gZwp9hiCg3j9QLS_2UHWqKU',
+  authDomain: 'house-marketplace-app-bc48c.firebaseapp.com',
+  projectId: 'house-marketplace-app-bc48c',
+  storageBucket: 'house-marketplace-app-bc48c.appspot.com',
+  messagingSenderId: '539128169823',
+  appId: '1:539128169823:web:b9a191d9606003d971e2ea',
+  measurementId: 'G-Q8X912KGSN',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore();
