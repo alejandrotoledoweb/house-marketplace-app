7b2a14b4f52129e342f70525558f3deba3887ac8
Alejandro Toledo <atoledofr@gmail.com>
2022-02-10 11:02:24 -0500

    add the name of the signin user in the profile component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    console.log(auth.currentUser);
+    setUser(auth.currentUser);
+  }, [auth]);
+  return user ? <h2>{user.displayName}</h2> : 'Not Found User';
 }
 
 export default Profile;
